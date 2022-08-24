c2122518856e7d533129ace6f7b1f90c631a04c7
Satish Patil <satish.patil7017@gmail.com>
2022-08-24 22:03:20 +0530

    [Satish] Added Ability to Analise Happy or Sad Problem Solution

---
+++ MoodAnalyerProblem/MoodAnalyerProblem/Mood.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyerProblem
+{
+    public class Mood
+    {
+        public string Happy_Mood = "Happy";
+        public string Sad_Mood = "Sad";
+
+        public string MoodAnalyser(string mood)
+        {
+            if(mood.Contains(Happy_Mood))
+            {
+                Console.WriteLine(Happy_Mood);
+                return Happy_Mood;
+            }
+            if (mood.Contains(Sad_Mood))
+            {
+                Console.WriteLine(Sad_Mood);
+                return Sad_Mood;
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+    }
+}
---
+++ MoodAnalyerProblem/MoodAnalyserProblemTests/TestCase1.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyerProblem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyerProblem.Tests
+{
+    [TestClass()]
+    public class TestCase1
+    {
+        [TestMethod()]
+        public void GivenHappyMoodMassage_ShouldRetuenHappy()
+        {
+            // Arrange
+            Mood mood = new Mood();
+            string ExpectedMood = "Happy";
+
+            //Act
+            string ActualMood = mood.MoodAnalyser("Happy");
+
+            //Asert
+            Assert.AreEqual(ExpectedMood, ActualMood);
+        }
+        public void GivenHappyMoodMassage_ShouldRetuenSad()
+        {
+            // Arrange
+            Mood mood = new Mood();
+            string ExpectedMood = "Sad";
+
+            //Act
+            string ActualMood = mood.MoodAnalyser("Sad");
+
+            //Asert
+            Assert.AreEqual(ExpectedMood, ActualMood);
+
+        }
+    }
+}
